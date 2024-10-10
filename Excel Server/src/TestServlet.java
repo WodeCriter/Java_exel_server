ab72014c17a617c659c30f86d44fd0d2ab562242
ItamarAlon <72469824+ItamarAlon@users.noreply.github.com>
2024-10-10 18:49:24 +0300

    Added new module

---
+++ Excel Server/src/TestServlet.java
@@ -0,0 +1,19 @@
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class TestServlet implements ServletContextListener
+{
+    @Override
+    public void contextInitialized(ServletContextEvent sce)
+    {
+        System.out.println("contextInitialized");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce)
+    {
+        System.out.println("contextDestroyed");
+    }
+}
