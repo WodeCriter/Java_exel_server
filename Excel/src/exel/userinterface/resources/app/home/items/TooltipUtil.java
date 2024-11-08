f243c962ed67bacaee580cd0706a45d607160406
ItamarAlon <72469824+ItamarAlon@users.noreply.github.com>
2024-11-08 21:49:20 +0200

    Now tells information about file when hovering

---
+++ Excel/src/exel/userinterface/resources/app/home/items/TooltipUtil.java
@@ -0,0 +1,61 @@
+package exel.userinterface.resources.app.home.items;
+
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
+import java.util.function.Function;
+
+public class TooltipUtil {
+
+    private static final Tooltip tooltip = new Tooltip();
+
+    static {
+        // Set default tooltip delay settings
+        tooltip.setShowDelay(Duration.millis(600));
+        tooltip.setHideDelay(Duration.millis(100));
+    }
+
+    public static <T> void setUpTooltip(ListView<T> listView, Function<T, String> tooltipTextProvider) {
+        // Configure the ListView's cell factory to add tooltips
+        listView.setCellFactory(lv -> {
+            ListCell<T> cell = new ListCell<>() {
+                @Override
+                protected void updateItem(T item, boolean empty) {
+                    super.updateItem(item, empty);
+
+                    // Clear previous content
+                    setText(null);
+                    setTooltip(null);
+
+                    if (!empty && item != null) {
+                        // Set cell text (you might want to customize this as needed)
+                        setText(item.toString()); // Customize display text if needed
+
+                        // Attach the tooltip to the cell
+                        setTooltip(tooltip);
+                    }
+                }
+            };
+
+            // Show tooltip when hovering over an item
+            cell.setOnMouseEntered(event -> {
+                if (!cell.isEmpty() && cell.getItem() != null) {
+                    showTooltip(event, cell.getItem(), tooltipTextProvider);
+                }
+            });
+
+            // Hide tooltip when not hovering
+            cell.setOnMouseExited(event -> tooltip.hide());
+
+            return cell;
+        });
+    }
+
+    private static <T> void showTooltip(MouseEvent event, T item, Function<T, String> tooltipTextProvider) {
+        tooltip.setText(tooltipTextProvider.apply(item)); // Set tooltip text based on the item
+        tooltip.show((ListView<?>) event.getSource(), event.getScreenX() + 10, event.getScreenY() + 10);
+    }
+}
+
---
+++ Exel Engine/src/engine/util/FileData.java
@@ -0,0 +1,63 @@
+package engine.util;
+
+import utils.perms.Permission;
+
+public class FileData
+{
+    private String filename;
+    private String ownerName;
+    private int numOfCols, numOfRows;
+    private Permission userPermission;
+
+    public FileData(String filename, String ownerName, int numOfCols, int numOfRows) {
+        this.filename = filename;
+        this.ownerName = ownerName;
+        this.numOfCols = numOfCols;
+        this.numOfRows = numOfRows;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public Permission getUserPermission() {
+        return userPermission;
+    }
+
+    public void setUserPermission(Permission userPermission) {
+        this.userPermission = userPermission;
+    }
+
+    public int getNumOfCols() {
+        return numOfCols;
+    }
+
+    public int getNumOfRows() {
+        return numOfRows;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    @Override
+    public String toString() {
+        return filename;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof FileData))
+            return false;
+        FileData obj = (FileData) o;
+
+        return filename.equals(obj.filename)
+                && ownerName.equals(obj.ownerName)
+                && numOfCols == obj.numOfCols
+                && numOfRows == obj.numOfRows;
+    }
+
+    public int compareTo(String fileName) {
+        return this.filename.compareTo(fileName);
+    }
+}
